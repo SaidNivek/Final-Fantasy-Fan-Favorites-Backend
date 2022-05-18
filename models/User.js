38db16871e114c4716223c6a4169a88e1979f598
SaidNivek <kevin.vi.dias@gmail.com>
2022-05-18 14:30:02 -0400

    Auth middleware added, JWT token working on user/me route

--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema ({
     email: {
         type: String,
         required: [true, "Please add an email"],
+        unique: true
     },
     likes: {
         type: Array,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,18 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const asyncHandler = require('express-async-handler')
+const { protect } = require('../middleware/authMiddleware')
 router.use(express.json())
 router.use(cors())
+
+// This function will generate a JWT
+// user id will be the payload
+const generateToken =(id) => {
+    // This will sign a new token with the id that's passed in, withthis secret used, and will expire in 1d
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '1d'
+    })
+} 
 
 const loginUser = asyncHandler(async (req, res) => {
     const {username, password} = req.body
@@ -18,7 +28,8 @@ const loginUser = asyncHandler(async (req, res) => {
         res.json({
             _id: user.id,
             username: user.username,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -30,28 +41,16 @@ const loginUser = asyncHandler(async (req, res) => {
 // Route /user/login
 router.post('/login', loginUser)
 
-// asyncHandler(async (req, res) => {
-//     const {username, password} = req.body
-//     // Check for user email
-//     const user = await User.findOne({username})
-//     // If the username exists by checking for their email AND
-//     // the hashed PW (stored in the DB) matches the PW given, then we can login    
-//     if(user && (await bcrypt.compare(password, user.password))) {
-//         res.json({
-//             _id: user.id,
-//             username: user.username,
-//             email: user.email
-//         })
-//     } else {
-//         res.status(400)
-//         throw new Error('Invalid credentials')
-//     }
-// }))
-
 // Get user data
 // Route /user/me
-router.get('/me', asyncHandler(async (req, res) => {
-    res.json({message: 'My user data'})
+// Private Access
+router.get('/me', protect, asyncHandler(async (req, res) => {
+    const { _id, username, email } = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        username: username,
+        email: email
+    })
 }))
 
 
@@ -86,7 +85,8 @@ router.post('/register', asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             username: user.username,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
